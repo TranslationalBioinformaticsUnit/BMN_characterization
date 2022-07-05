c7fa1767b63440e83200aff0e54d4f29e5482572
K10308 <33249929+K10308@users.noreply.github.com>
2022-07-05 10:17:41 +0300

    Update Using_Bootstrapping_functions.R

--- R_code/Using_Bootstrapping_functions.R
+++ R_code/Using_Bootstrapping_functions.R
@@ -20,21 +20,15 @@ n.runs =10 ### repeat the process for 10 times
 ##  idents: Identities of each cell from seurat object for testing
 mEC_rf_step1 <- Pair_select(EC.integration, idents=EC.integration$bootstrapped)  
 
-### Function 2 rf_preplot: to get metrix iike recall of every cell for plotting #####
-##  rf_step1_result: result from Pair select function 
-##  obj: same Seurat object for Pair_selection
-mEC_rf_preplot <- rf_preplot(mEC_rf_step1, EC.integration)
-
-#add recall to the seurat object
-EC.integration$recall_per_cell <- mEC_rf_report$pred_real_sum$recall 
+### Function 2 add_recall_per_cell: add plotting matrix "recall_per_cell" to the suerat object
+EC.integration <- add_recall_per_cell(rf_step1_result = mEC_rf_step1,obj = EC.integration,
+                                      seurat_colname = "recall_per_cell")
 
 ### function 3 Dominant_cluster: determine the dominant cluster for each run #####
-
 mEC_Dominant <- Dominant_cluster(mEC_rf_step1)
 
-###function 5: how many times a clusters remains dominant cluster for cells inside in all runs #####
-EC.integration<- stable_times(obj = EC.integration,obj_dominant =mEC_Dominant) ## adding no. of stable times to Seurat object
-
+###function 4: how many times a clusters remains dominant cluster for cells inside in all runs #####
+EC.integration<- stable_times(obj = EC.integration,obj_dominant =mEC_Dominant,meta.names = "stable_times") ## adding no. of stable times to Seurat object meta.names: seurat col names to store no. of stable times
 
 # PLOTING
 
@@ -43,44 +37,19 @@ DimPlot(EC.integration,group.by = "bootstrapped", label = T )+RotatedAxis()+ lab
 #plot recall per cell in UMAP
 FeaturePlot(EC.integration, features = "recall_per_cell", min.cutoff = 0, max.cutoff = 1)+scale_colour_viridis_c(begin = 0.2)+ RotatedAxis()
 #plot the stable times
-EC.integration@meta.data %>% dplyr::select(stable_times_of_13_comparisons,bootstrapped) %>% table() %>% 
-  data.frame() %>% mutate(stable_times_of_13_comparisons= factor(stable_times_of_13_comparisons)) %>% 
-  ggplot()+geom_bar(aes(fill=stable_times_of_13_comparisons, y = Freq, x=bootstrapped),position="fill", stat = "identity")+
+EC.integration@meta.data %>% dplyr::select(stable_times,bootstrapped) %>% table() %>% 
+  data.frame() %>% mutate(stable_times= factor(sstable_times)) %>% 
+  ggplot()+geom_bar(aes(fill=stable_times, y = Freq, x=bootstrapped),position="fill", stat = "identity")+
   scale_fill_viridis_d(begin = 0.2)+theme_bw()+RotatedAxis()
 
 ### dominant plot:
 Plot_dominant_umap(pdf_prefix = "mEC", EC.integration, idents = EC.integration$bootstrapped, obj_dominant  = mEC_Dominant, output.dir = "~/Downloads")
 
+
 ####  Assign unstable cluster 
 ####  using cluster A2 as an example: subcluster A2.2 and cluster A2.3 is not stable, will be assigned to the rest of the clusters 
-
-names <- levels(Cluster_A2@active.ident)
-#names[1] is A2.3, names[5] is A2.2
-obj_sub_train <- subset(Cluster_A2, idents=names[c(2,3,4,6,7)])
-obj_sub_test <- subset(Cluster_A2, idents=names[1])
-n.bin=5
-n.gene = 50 ### use 50 genes to perform random forest
+Cluster_A2 <- assign_unstable_clusters(obj = Cluster_A2,unstable_clusters = c("A2.2", "A2.3"),idents = Cluster_A2@active.ident,no.bins = 5,no.genes  = 50)
 
 
-### function 6: assign unstable clusters ######
-##  obj_sub_train: clusters use as a referencing (training dataset)
-##  obj_sub_test: unstable cluster to be assigned to clusters in obj_sub_train
-##  n1: cluster names in obj_sub_train
-##  n2: cluster names in obj_sub_test
 
-### Assign subcluster A2.3 to the rest 
-A2_3_list <- pair_rf_assign(obj_sub_test = obj_sub_test,obj_sub_train=obj_sub_train, n1=paste0(names[c(2,3,4,6,7)],collapse=""), n2=names[1])
-A2_3_list$pred.table$most_classified  <- apply(A2_3_list$pred.table, 1, function(x)names(which.max(table(t(x))))) ### chose the new cluster names that has been assigned most to each cell 
-
-### Assign subcluster A2.3 to the rest 
-obj_sub2 <- subset(Cluster_A2, idents=names[5])
-A2_2_list <- pair_rf_assign(obj_sub2,obj_sub_train, n1=paste0(names[c(2,3,4,6,7)],collapse=""), n2=names[5])
-A2_2_list$pred.table$most_classified  <- apply(A2_2_list$pred.table, 1, function(x)names(which.max(table(t(x)))))
-
-#### adding metadata to cluster A2 
-PC65 <- c(A2_3_list$pred.table$most_classified, A2_2_list$pred.table$most_classified)
-names(PC65) <- c(rownames(A2_3_list$pred.table), rownames(A2_2_list$pred.table)) ### new identity for cluster A2.2 and clusterA2.3
-PC67 <- Cluster_A2$PC67[-which(colnames(Cluster_A2)%in%names(PC65))] ### remove old identity for cells from cluster A2.2 and cluster A2.3
-PC65 <- c(PC65, PC67) ### adding new identity of cells from cluster A2.2 and cluster A2.3
-Cluster_A2 <- AddMetaData(Cluster_A2, PC65, col.name = "PC65") ### adding new identity to the seurat object
 
